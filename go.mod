45f8fef9a4ba0af7dff73bb569995540274a7b07
Pawandeep Singh <pawandeepse@me.com>
2025-05-27 13:45:51 +0800

    Initial Hello CV Camera

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module blink-moji
 
 go 1.24.1
+
+require gocv.io/x/gocv v0.41.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"gocv.io/x/gocv"
+)
 
 func main() {
 	fmt.Println("Hello, 👁️ Blink  😀 Moji  !")
+
+	// open webcam
+	webcam, err := gocv.VideoCaptureDevice(0)
+	if err != nil {
+		log.Fatalf("Error opening video capture device: %v\n", err)
+		return
+	}
+	defer webcam.Close()
+
+	// Create a window to display the video
+	window := gocv.NewWindow("Hello Blink Moji")
+	defer window.Close()
+
+	// create image matrix to hold the video frame
+	img := gocv.NewMat()
+	defer img.Close()
+	for {
+		if ok := webcam.Read(&img); !ok {
+			log.Println("Error reading from webcam")
+			return
+		}
+		if img.Empty() {
+			log.Println("No image captured from webcam")
+			continue
+		}
+
+		window.IMShow(img) // display the image in the window
+
+		if window.WaitKey(1) >= 0 {
+			fmt.Println("Exiting...")
+			break
+		}
+	}
 }
